96a41834d447e2ef3cee09186e0f36d4e16c48a7
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-04-06 22:39:17 -0500

    started writing template for md generation, will move to seperate js file later, included util to write file async

--- index.js
+++ index.js
@@ -1,73 +1,123 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const genMD = require('./utils/generateMarkdown.js');
 const util = require('util');
 
 // // Creating a promise to write file
-// const writeFileAsync = util.promisify(fs.writeFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // // TODO: Create an array of questions for user input
-// const promptUser = () => {
-//  return inquirer.prompt([
-inquirer
-    .prompt([
+const promptUser = () => {
+ return inquirer.prompt([
+// inquirer
+//     .prompt([
     {
         name: 'title',
         type: 'input',
-        question: 'What is the title of your project?',
+        message: 'What is the title of your project?',
     },
     {
         name: 'description',
         type: 'input',
-        question: 'Please provide a breif description of your application and the functionality.',
+        message: 'Please provide a breif description of your application and the functionality.',
     },
     {
         name: 'installation',
         type: 'input',
-        question: 'Please provide detailed installation instructions for you application.',
+        message: 'Please provide detailed installation instructions for you application.',
     },
     {
         name: 'usage',
         type: 'input',
-        question: 'Please provide any usage information for the user.'
+        message: 'Please provide any usage information for the user.'
     },
     {
         name: 'contributing',
         type: 'input',
-        question: 'What are the conritbutor guidlines?',
+        message: 'What are the conritbutor guidlines?',
     },
     {
         name: 'license',
         type: 'list',
-        question: 'Please select the license used.',
-        choices: ['MIT', 'GPLv3', 'GPLv2', 'Apache', 'BSD 3'],
+        message: 'Please select the license used.',
+        choices: ['MIT', 'GPLv3', 'GPLv2', 'Apache', 'BSD 3', 'Other'],
     },
     {
         name: 'test',
-        type: '',
-        question: '',
+        type: 'input',
+        message: 'Please provide any information regarding tests on your application.',
     },
     {
-        name: '',
-        type: '',
-        question: '',
+        name: 'email',
+        type: 'input',
+        message: 'What is your email address?',
+    },
+    {
+        name: 'linkedin',
+        type: 'input',
+        message: 'Please provide your LinkedIn URL.',
     },
     {
         name: 'github',
         type: 'input',
-        question: 'Please provide your Github username(case sensitive).',
+        message: 'Please provide your Github username(case sensitive).',
     },
  ])
-// };
-// // TODO: Create a function to write README file
+};
+
+const generateMD = (answers) => 
+`# ${answers.title}
+    
+    
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#test)
+- [Questions](#questions)
+
+
+## Description
+${answers.description}
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contributing
+${answers.contributing}
+
+## License
+${answers.license}
+
+## Tests
+${answers.tests}
+
+## Questions
+
+  If you have any questions regarding the repo, please contact me at one of the following:
+    [Github](https://github.com/${answers.github})
+    [Email](mailto:${answers.email})
+    [LinkedIn](${answers.linkedin})
+`;
+
+// TODO: Create a function to write README file
 // function writeToFile(fileName, data) {};
 
-// // TODO: Create a function to initialize app
-// function init = () => {
-//     promptUser()
-//     .then()
+// TODO: Create a function to initialize app
+const init = () => {
+    promptUser()
+    .then((answers) => console.log(answers))
+    .then((answers) => writeFileAsync('./generated/README.md', generateMD(answers)))
+    .then(() => console.log('README.md successfully generated!'))
+    .catch((err) => console.error(err))
+};
 
-// };
 
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
